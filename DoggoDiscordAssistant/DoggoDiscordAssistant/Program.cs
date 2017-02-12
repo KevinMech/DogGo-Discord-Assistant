ae5e263c7750f685d5f116ca0a27050e2dddcdc9
Kevin <KevinMech@users.noreply.github.com>
2017-02-12 02:05:19 -0500

    Added Logging class that logs any information about the bot and outputs to the console

--- DoggoDiscordAssistant/DoggoDiscordAssistant/Program.cs
+++ DoggoDiscordAssistant/DoggoDiscordAssistant/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 
 namespace DoggoDiscordAssistant
 {
@@ -15,6 +16,8 @@ namespace DoggoDiscordAssistant
                 metadata.AppName = "DogGo Discord Assistant";
                 metadata.AppVersion = "0.1.0";
                 metadata.AppUrl = "https://github.com/KevinMech/DogGo-Discord-Assistant";
+                metadata.LogLevel = LogSeverity.Error;
+                metadata.LogHandler += Logging.APIErrorLogHandling;
             });
         }
     }
