dd5be85fddc0dbc56c6d49afb3a24fb813dc8ddb
Kevin <KevinMech@users.noreply.github.com>
2017-02-12 01:45:40 -0500

    Created basic structure for program and added new class for bot

---
+++ DoggoDiscordAssistant/DoggoDiscordAssistant/DoggoDiscordAssistant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace DoggoDiscordAssistant
+{
+    class DoggoDiscordAssistant : DiscordClient
+    {
+        public DoggoDiscordAssistant(Action<DiscordConfigBuilder> configFunc) : base (configFunc)
+        {
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ DoggoDiscordAssistant/DoggoDiscordAssistant/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoggoDiscordAssistant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoggoDiscordAssistant doggodiscordassistant = new DoggoDiscordAssistant(metadata =>
+            {
+                metadata.AppName = "DogGo Discord Assistant";
+                metadata.AppVersion = "0.1.0";
+                metadata.AppUrl = "https://github.com/KevinMech/DogGo-Discord-Assistant";
+            });
+        }
+    }
+}
