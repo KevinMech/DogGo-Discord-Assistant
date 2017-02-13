aa4741487bf15be63eace4704c40f6c4f2b172de
Kevin <KevinMech@users.noreply.github.com>
2017-02-13 08:06:31 -0500

    Created new Abstarct class called Command

---
+++ DoggoDiscordAssistant/DoggoDiscordAssistant/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoggoDiscordAssistant
+{
+    abstract class Command
+    {
+        public abstract string CommandName { get; set; }
+        public abstract string Description { get; set; }
+        public abstract string Help { get; set; }
+
+        public abstract void Execute();
+    }
+}
