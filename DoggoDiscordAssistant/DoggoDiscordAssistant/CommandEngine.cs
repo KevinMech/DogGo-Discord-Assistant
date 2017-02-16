24b7cb5fdb75f304bfd1726d5afbd5a974d199c8
Kevin <KevinMech@users.noreply.github.com>
2017-02-16 00:13:49 -0500

    Fixed forgot to push CommandEngine.cs. Whoops!

---
+++ DoggoDiscordAssistant/DoggoDiscordAssistant/CommandEngine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoggoDiscordAssistant
+{
+    class CommandEngine
+    {
+        public static List<Command> AvailableCommands { get; private set; }
+
+        /// <summary>
+        /// Queries the input of users chat and retrieves the command, flag and flag parameters
+        /// </summary>
+        /// <param name="message"></param>
+        public static void parseInput(string message)
+        {
+            //Check to see if message starts with command symbol.
+            if (message[0] == '>')
+            {
+                string[] splitMessage = message.Split(' ');
+                foreach(Command command in AvailableCommands)
+                {
+                    foreach(String word in splitMessage)
+                    {
+                        if (word == command.Identifier) command.Execute();
+                    }
+                }
+            }
+        }
+    }
+}
