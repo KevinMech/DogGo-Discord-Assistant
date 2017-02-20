db7ad7e0758d89a98836de5975bc57f58b00c49c
Kevin <KevinMech@users.noreply.github.com>
2017-02-19 22:21:28 -0500

    Created a new Server class to keep track of each server

---
+++ DoggoDiscordAssistant/DoggoDiscordAssistant/Server.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DoggoDiscordAssistant
+{
+    class Server
+    {
+        public Discord.Server ServerAPI { get; }
+        public string Welcome { get; }
+        public string Leave { get; }
+
+        public Server(Discord.Server server)
+        {
+            /* Since discords server class is not inheritable due to design restrictions (internal constructor), 
+            the discord server object will be encaspulated within this server class */
+            ServerAPI = server;
+
+            //Set default welcome message
+            Welcome = "Welcome to " + ServerAPI.Name + "!";
+        }
+
+    }
+}
